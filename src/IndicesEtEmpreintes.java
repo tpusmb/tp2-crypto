b40968537e6fce134e96fbebb3488bd1b9102985
Swan <swan.blanc.pro@gmail.com>
2019-10-08 16:52:44 +0200

    Add working version but bug with 142678997 -> Salut

--- src/IndicesEtEmpreintes.java
+++ src/IndicesEtEmpreintes.java
@@ -2,11 +2,28 @@ import java.util.Arrays;
 
 public class IndicesEtEmpreintes {
 
-    public IndicesEtEmpreintes(int taille_min, int taille_max) {
-        int[] min_val;
-        int[] max_val;
-        for(int i = taille_min; i < taille_max; i++){
+    private double[] min_val;
+    private double[] max_val;
+    private int[] nb_lettre;
+    private double[] const_minus;
+    private String alphabet;
 
+    public IndicesEtEmpreintes(String alphabet, int taille_min, int taille_max) {
+        this.alphabet = alphabet;
+        int nb_test = (taille_max - taille_min) + 1;
+        this.min_val = new double[nb_test];
+        this.max_val = new double[nb_test];
+        this.const_minus = new double[nb_test];
+        this.nb_lettre = new int[nb_test];
+        double acc = 0;
+        int nb_lettre_acc = taille_min;
+        for(int i = 0; i < nb_test; i++){
+            min_val[i] = Math.pow(alphabet.length(), nb_lettre_acc);
+            max_val[i] = Math.pow(alphabet.length(), nb_lettre_acc + 1);
+            const_minus[i] = acc;
+            nb_lettre[i] = nb_lettre_acc;
+            acc += Math.pow(alphabet.length(), nb_lettre_acc);
+            nb_lettre_acc++;
         }
     }
 
@@ -17,4 +34,23 @@ public class IndicesEtEmpreintes {
 
     }
 
+
+    public String i2c(int i){
+
+        int norm_i = i;
+        int nb_lettre = 0;
+        for(int tab_index = 0; tab_index < this.max_val.length; tab_index++){
+            if(this.min_val[tab_index] <= i && i < this.max_val[tab_index]){
+                norm_i -= this.const_minus[tab_index];
+                nb_lettre = this.nb_lettre[tab_index];
+                break;
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        for(int loop = 0; loop < nb_lettre; loop++){
+            res.append(this.alphabet.charAt(norm_i % this.alphabet.length()));
+            norm_i /= this.alphabet.length();
+        }
+        return res.reverse().toString();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Menu options:");
         System.out.println("1. Hash MD5");
-        System.out.println("2. Some menu option");
+        System.out.println("2. test i2c");
         System.out.println("3. Some menu option");
         System.out.println("4. Some menu option");
         System.out.println("5. Exit the program");
@@ -34,14 +34,21 @@ public class Main {
 
     private static void process(int choice) throws Exception {
         PropertyValues propertyValues = new PropertyValues();
-        System.out.println(propertyValues.getPropValues("taille_min"));
-        System.out.println(propertyValues.getPropValues("taille_max"));
+        int taille_min = Integer.parseInt(propertyValues.getPropValues("taille_min"));
+        int taille_max = Integer.parseInt(propertyValues.getPropValues("taille_max"));
+        taille_min = Math.min(taille_min, taille_max);
+        taille_max = Math.max(taille_min, taille_max);
+        String alphabet = propertyValues.getPropValues("alphabet");
 
         switch (choice) {
             case 1:
                 System.out.println("Hash MD5 of \"" + propertyValues.getPropValues("testtext") +
                         "\": " + Hashage.hashMD5(propertyValues.getPropValues("testtext")));
+                break;
             case 2:
+                IndicesEtEmpreintes indicesEtEmpreintes = new IndicesEtEmpreintes(alphabet, taille_min, taille_max);
+                int i2c_id = Integer.parseInt(propertyValues.getPropValues("test_i2c"));
+                System.out.println("I2C test with id " + i2c_id + ": " + indicesEtEmpreintes.i2c(i2c_id));
 
         }
     }
