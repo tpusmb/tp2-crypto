e4bee1e6b365e03da017fe7c6de531369366b671
Swan <swan.blanc.pro@gmail.com>
2019-10-08 14:28:14 +0200

    clean version

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         PropertyValues propertyValues = new PropertyValues();
         System.out.println(propertyValues.getPropValues("taille_min"));
+        System.out.println(propertyValues.getPropValues("taille_max"));
     }
 }
