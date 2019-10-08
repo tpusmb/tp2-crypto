46337ac33ebfc69b64af23883d4f9064da97df9e
ClemiNeko <c.lebras776@laposte.net>
2019-10-08 14:58:51 +0200

    Corrected.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+    public static void main(String[] args) throws Exception {
         PropertyValues propertyValues = new PropertyValues();
         System.out.println(propertyValues.getPropValues("taille_min"));
         System.out.println(propertyValues.getPropValues("taille_max"));
+
+        System.out.println(Hashage.hashMD5(propertyValues.getPropValues("testtext")));
     }
 
 }
